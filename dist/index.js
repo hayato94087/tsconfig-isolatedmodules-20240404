63e92c802fa294eb055353c4194dad4267fa6346
hayato94087 <hayato94087@gmail.com>
2024-04-03 05:32:00 +0900

    feat:isolatedModulesを無効化した場合の挙動を確認

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const user = {
+    name: 'John',
+    age: 30,
+};
+console.log(user);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,8 @@
+import { User } from './re-export';
+
+const user: User = {
+  name: 'John',
+  age: 30,
+};
+
+console.log(user);
